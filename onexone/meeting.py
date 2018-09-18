81750340d4ad571accc77a2d49ed5b232a895dab
Michael Davies <michael@the-davies.net>
2018-09-18 15:59:41 +0930

    Added meeting deletion

--- onexone/meeting.py
+++ onexone/meeting.py
@@ -13,6 +13,7 @@ class Meeting:
     def __init__(self):
         self.c = command.CommandOptions('meeting')
         self.c.add_command('add', self.add, "<person> <date>")
+        self.c.add_command('delete', self.delete, "<person> <date>")
         self.c.add_command('up-next', self.up_next, "")
         self.p = person.Person()
 
@@ -37,6 +38,34 @@ class Meeting:
         ds = datastore.get_datastore()
         dictionary = ds.get_dict()
         dictionary[matches[0]]['meetings'].append(meeting)
+        ds.save()
+
+    @debugging.trace
+    def delete(self, args):
+        len_args = len(args)
+        if len_args != 2:
+            self.c.display_usage('delete')
+            return
+
+        name = args[0]
+        meeting = args[1]
+        matches = self.p._find([name], False)
+        if not matches:
+            print("Can't find '{}'".format(name))
+            return
+        if len(matches) > 1:
+            print("Multiple persons found: {}".format(matches))
+            return
+        # Note(mrda): Shouldn't be playing with the internals of
+        # entries here.  We need code to abstract this
+        ds = datastore.get_datastore()
+        dictionary = ds.get_dict()
+        all_meetings = dictionary[matches[0]]['meetings']
+        if meeting not in all_meetings:
+            print("Couldn't find {} in {}'s list of meetings".format(meeting, person))
+            return
+        cleaned = [ x for x in all_meetings if x != meeting ]
+        dictionary[matches[0]]['meetings'] = cleaned
         ds.save()
 
     @debugging.trace
