bf7c06871ae2bdca29efbead8c825de8aded33e6
Michael Davies <michael@the-davies.net>
2018-09-28 12:25:31 +0930

    Adding test framework

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -9,6 +9,12 @@ class TestDataStore(unittest.TestCase):
         self.ds._ds_version['minor'] = 6
         self.ds._ds_version['patch'] = 3
 
+    # TODO(mrda): Tests for new_entry
+
+    # TODO(mrda): Tests for remove_entry
+
+    # TODO(mrda): Tests for version functions
+
     def test_version(self):
         self.assertEquals('9.6.3', self.ds.version())
 
@@ -17,3 +23,23 @@ class TestDataStore(unittest.TestCase):
 
     def test_minor_version(self):
         self.assertEquals(6, self.ds.minor_version())
+
+    # TODO(mrda): Tests for list_keys
+
+    # TODO(mrda): Tests for list_everything
+
+    # TODO(mrda): Tests for get_dict
+
+    # TODO(mrda): Tests for get_value
+
+    # TODO(mrda): Tests for set_enabled
+
+    # TODO(mrda): Tests for get_all_fullnames
+
+    # TODO(mrda): Tests for get_meetings
+
+    # TODO(mrda): Tests for save
+
+    # TODO(mrda): Tests for load
+
+    # TODO(mrda): Tests for dump
--- tests/test_person.py
+++ tests/test_person.py
@@ -6,6 +6,8 @@ class TestPerson(unittest.TestCase):
     def setUp(self):
         self.p = onexone.person.Person()
 
+    # TODO(mrda): Tests for _is_match
+
     def test__is_match_matches(self):
         self.assertTrue(self.p._is_match('banana', 'banana'))
         self.assertTrue(self.p._is_match('bananarama', 'banana'))
@@ -13,4 +15,34 @@ class TestPerson(unittest.TestCase):
     def test__is_match_doesnt_match(self):
         self.assertFalse(self.p._is_match('banana', 'bananarama'))
 
-    # TODO(mrda): Insert many more tests here
+    # TODO(mrda): Tests for _search
+
+    # TODO(mrda): Tests for _find
+
+    # TODO(mrda): Tests for find_person
+
+    # TODO(mrda): Tests for _exact_match
+
+    # TODO(mrda): Tests for find
+
+    # TODO(mrda): Tests for is_anabled
+
+    # TODO(mrda): Tests for info
+
+    # TODO(mrda): Tests for _print_person
+
+    # TODO(mrda): Tests for _build_filename
+
+    # TODO(mrda): Tests for _new_person
+
+    # TODO(mrda): Tests for add
+
+    # TODO(mrda): Tests for delete
+
+    # TODO(mrda): Tests for info
+
+    # TODO(mrda): Tests for list
+
+    # TODO(mrda): Tests for enable
+
+    # TODO(mrda): Tests for parse
